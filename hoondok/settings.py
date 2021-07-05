953c55e54963658db73d77feda108dc5b23dd62c
pkaramagi <pkaramagi1@gmail.com>
2021-07-05 19:29:46 +0300

    Update settings.py

--- hoondok/settings.py
+++ hoondok/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-c#%05v3h&2-&7g5)25+2t9c%432ma2$7n_=6upd%p5ut1txi&'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
